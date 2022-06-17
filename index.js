55c7a19d57f54860df72214a26f600ade3838495
Oséias astor evangelista de Freitas <oseiasdfreitas@gmail.com>
2022-06-17 18:37:57 -0300

    Fixing the hello method and my lib name.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-exports.hello = () => {
+const fs = require('fs');
+const dir = "src/test.vue";
+
+module.exports.hello = () => {
+  if (!fs.existsSync(dir)){
+    //Efetua a criação do diretório
+    fs.mkdirSync(dir);
+   }
   console.log('Just a test.');
 };
