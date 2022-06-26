7b05aabd0cd438988a08c97f5959cfe790e31c7d
Oséias astor evangelista de Freitas <oseiasdfreitas@gmail.com>
2022-06-26 17:54:28 -0300

    Fixing my lib.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const fs = require('fs');
+const {writeFile} = require('fs/promises');
 const newPageDir = "pages/test.vue";
-
-module.exports.hello = () => {
-  console.log('Just a test.');
-};
+const entry = require('prompt-sync')({sigint: true});
+let vueFile = 'index.vue';
+let vueFileContent = '<template><h1>Test</h1></template>';
+let jsFile = 'script.js';
+let jsFileContent = 'var test = null;';
+let value;
 
 module.exports.makePage = (pageName) => {
    fs.mkdirSync(`pages/${pageName}`);
+   writeFile(vueFile, vueFileContent).then(console.log).catch(console.error);
+   writeFile(jsFile, jsFileContent).then(console.log).catch(console.error);
 };
+
+// TODO:
+// module.exports.make = (amount) => {
+//   value = entry('Digite a quantidade de pastas: ');
+// console.log(value);
+//   if (amount === 1) {
+    
+//   } else {
+    
+//   } 
+// };
