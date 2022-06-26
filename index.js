45822f9dbe0b528872e0ba0277f7d89420a92c49
Oséias astor evangelista de Freitas <oseiasdfreitas@gmail.com>
2022-06-26 18:18:19 -0300

    Fixing.

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const {writeFile} = require('fs/promises');
 const newPageDir = "pages/test.vue";
 const entry = require('prompt-sync')({sigint: true});
 let vueFile = 'index.vue';
-let vueFileContent = '<template><h1>Test</h1></template>';
+let vueFileContent = '<template><h1>Test</h1></template><script type="text/javascript" src="./script.js" /><style scoped type="text/css" src="./style.css" />';
 let jsFile = 'script.js';
-let jsFileContent = 'var test = null;';
+let jsFileContent = "import { mapActions, mapState } from 'vuex' ; export default {}";
 let value;
 
 module.exports.makePage = (pageName) => {
-   var path = `pages/${pageName}`;
+   var path = `pages/${pageName}/`;
    fs.mkdirSync(`pages/${pageName}`);
-   writeFile(path + vueFile, vueFileContent).then(console.log).catch(console.error);
-   writeFile(path + jsFile, jsFileContent).then(console.log).catch(console.error);
+   writeFile(path + vueFile, vueFileContent).then(() => {console.log('sucess!!')})
+   .catch((error) => {console.error('Error!!')});
+   writeFile(path + jsFile, jsFileContent).then(() => {console.log('sucess!!')})
+   .catch((error) => {console.error('Error!!')});
 };
 
 // TODO:
