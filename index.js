2f59d8ee4d9381e906ae1ce27bf469b198f5a98d
Oséias astor evangelista de Freitas <oseiasdfreitas@gmail.com>
2022-06-26 18:25:19 -0300

    Fix

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const {writeFile} = require('fs/promises');
 const newPageDir = "pages/test.vue";
 const entry = require('prompt-sync')({sigint: true});
 let vueFile = 'index.vue';
-let vueFileContent = '<template><h1>Test</h1></template><script type="text/javascript" src="./script.js" /><style scoped type="text/css" src="./style.css" />';
+let vueFileContent = '<template><br><h1>Test</h1><br></template><br><br><script type="text/javascript" src="./script.js" /><br><style scoped type="text/css" src="./style.css" />';
 let jsFile = 'script.js';
-let jsFileContent = "import { mapActions, mapState } from 'vuex' ; export default {}";
+let jsFileContent = "import { mapActions, mapState } from 'vuex' ;<br> export default {}";
 let value;
 
 module.exports.makePage = (pageName) => {
