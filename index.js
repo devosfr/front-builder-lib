0796e5029488f2379b2ec63b185077685f090f03
Oséias astor evangelista de Freitas <oseiasdfreitas@gmail.com>
2022-06-26 18:36:28 -0300

    Fixing

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const entry = require('prompt-sync')({sigint: true});
 let vueFile = 'index.vue';
 let vueFileContent = '<template>\n<h1>Test</h1>\n</template>\n\n<script type="text/javascript" src="./script.js" />\n<style scoped type="text/css" src="./style.css" />';
 let jsFile = 'script.js';
-let jsFileContent = "import { mapActions, mapState } from 'vuex' ;\n export default {}";
+let jsFileContent = "import { mapActions, mapState } from 'vuex' ;\n\n export default {}";
+let styleFile = 'style.css';
 let value;
 
 module.exports.makePage = (pageName) => {
@@ -13,7 +14,11 @@ module.exports.makePage = (pageName) => {
    fs.mkdirSync(`pages/${pageName}`);
    writeFile(path + vueFile, vueFileContent).then(() => {console.log('sucess!!')})
    .catch((error) => {console.error('Error!!')});
+  
    writeFile(path + jsFile, jsFileContent).then(() => {console.log('sucess!!')})
+   .catch((error) => {console.error('Error!!')});
+
+   writeFile(path + styleFile, '').then(() => {console.log('sucess!!')})
    .catch((error) => {console.error('Error!!')});
 };
 
