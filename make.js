77e0696588f73d511e4e0905a116aae4a643fdba
Oseias <oseias@obi.tec.br>
2023-09-24 13:26:51 -0300

    Code fix.

--- make.js
+++ make.js
@@ -1,22 +1,24 @@
 const Build = require('@oseiasdev/front-builder');
-const entry = require('prompt-sync')({sigint: true});
-let value= null;
+const entry = require('prompt-sync')({
+  sigint: true
+});
+let value = null;
 let main = null;
 let type = null;
 
 value = entry('Git a name to the folder: ');
 console.log('Hierarchy: ' + value);
 
-main = entry('Where doyou want to create? (for example pages, components...): ');
+main = entry('Where do you want to create? (for example pages, components...): ');
 console.log('Main folder: ' + main);
 
 type = entry('Write js to javascript or ts to typescript (js/ts): ');
 console.log('Type picked: ' + type);
 
 if (type === 'js') {
- Build.jsMakePage(value, main);
+  Build.jsMakePage(value, main);
 } else if (type === 'ts') {
- Build.tsMakePage(value, main);
+  Build.tsMakePage(value, main);
 } else {
   console.log('Chose a correct type, please..');
   type = entry('Write js to javascript or ts to typescript (js/ts): ');
